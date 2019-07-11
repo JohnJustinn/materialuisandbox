534d93f13639eadc40c8a895a63e385275879eef
Johnathan Justinn <jjustinn85@gmail.com>
2019-07-11 13:58:03 -0400

    Header Ex Imports

---
+++ materialuiexample/src/components/NavHeaderEx.js
@@ -0,0 +1,5 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
