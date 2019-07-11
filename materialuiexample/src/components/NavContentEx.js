520282b4d340bd1a72dba3f1db310ce55c0daace
Johnathan Justinn <jjustinn85@gmail.com>
2019-07-11 13:54:03 -0400

    List variable completed

--- materialuiexample/src/components/NavContentEx.js
+++ materialuiexample/src/components/NavContentEx.js
@@ -5,3 +5,38 @@ import ListItemText from "@material-ui/core/ListItemText";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import Icon from "@material-ui/core/Icon";
 import Divider from "@material-ui/core/Divider";
+
+const list = [
+    {
+      primaryText: "My Files",
+      icon: "folder"
+    },
+    {
+      primaryText: "Shared with me",
+      icon: "people"
+    },
+    {
+      primaryText: "Starred",
+      icon: "star"
+    },
+    {
+      primaryText: "Recent",
+      icon: "schedule"
+    },
+    {
+      primaryText: "Offline",
+      icon: "offline_pin"
+    },
+    {
+      primaryText: "Uploads",
+      icon: "publish"
+    },
+    {
+      primaryText: "Backups",
+      icon: "backup"
+    },
+    {
+      primaryText: "Trash",
+      icon: "delete"
+    }
+  ];
